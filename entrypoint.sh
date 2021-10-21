28840f4093f8c368e068b6e1177417abf9b86cda
toddvalentine-fc <81376899+toddvalentine-fc@users.noreply.github.com>
2021-10-21 16:04:32 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "checking tasks..."
 
-if [ "${GITHUB_EVENT_NAME}" != "push" ]; then
-    echo "This action only runs on push"
-    echo "Found: ${GITHUB_EVENT_NAME}"
-    exit 1
-fi
-
 RESULT=$(curl https://cronitor.io/api/monitors -u ${INPUT_CRONITOR_KEY}: | jq -c -r '.monitors[] | [.name, .running] | "\(.[0]) \(.[1])"')
 while read -r -a arr; do 
     echo "task: ${arr[0]} | status: ${arr[1]}"
